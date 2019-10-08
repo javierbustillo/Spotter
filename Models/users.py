40cf43583170b0a8d0fde4713e7faf4ad34f7db2
javierbustillo <javier.bustillo@upr.edu>
2019-10-07 22:24:30 -0400

    Add log

--- Models/users.py
+++ Models/users.py
@@ -8,7 +8,7 @@ class Users(Model):
 
     def create_user(self, spotify_id, access_token, refresh_token):
         cursor = self.get_cursor()
-        print('Spotify_id: %s\n\n' % spotify_id)
+        print('Spotify_id: %s, %s\n\n' % spotify_id, type(spotify_id))
         print('Access_token: %s\n\n' % access_token)
         print('refresh_token: %s \n\n' % refresh_token)
         query = 'INSERT INTO users (spotify_id, access_token, refresh_token) VALUES (%s,%s,%s)'
