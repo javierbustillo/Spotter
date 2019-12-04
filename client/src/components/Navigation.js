bb84f9b871633f58208b9aa398d50233d5ca5850
Gabriel Rosario <gabriel.rosario2@upr.edu>
2019-12-03 22:02:15 -0400

    login flow change

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -17,6 +17,16 @@ class Navigation extends Component{
         }
     }
 
+    loginButton(){
+
+        var url = "https://accounts.spotify.com/authorize?client_id=814daec5d00647a6951025d3ba7abc3a&redirect_uri=http:%2F%2Flocalhost:3000%2Fuser&scope=user-read-private%20user-read-email%20user-top-read%20user-read-playback-state%20user-follow-modify%20user-library-read%20user-library-modify&response_type=token&state=123"
+        return(
+            <a href={url}> 
+                        <Button className="login-button" color="primary">Login</Button>
+            </a>
+        )
+    }
+
     
 
     render(){
@@ -24,9 +34,10 @@ class Navigation extends Component{
             <div className="navbar-container">
                 <Navbar className="navbar-header">
                     <NavLink href="/" className="navbar-text">Spotter</NavLink>
-                    <a href="http://localhost:8888">
+                    {/* <a href="http://localhost:8888">
                         <Button className="login-button" color="primary">Login</Button>
-                    </a>                
+                    </a>                 */}
+                    {this.loginButton()}
                 </Navbar>
             </div>
         )
